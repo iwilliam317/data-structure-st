fac12313ff12d303854a130bdb5ceb1cf5b7810a
Shinji Obana <willshinji@gmail.com>
2020-03-29 19:01:59 +0100

    bloom filter

---
+++ files/bloom_filter.js
@@ -0,0 +1,13 @@
+# Linked List
+
+## Description
+* Similar to Hash Map
+* Statistic data structure
+* Store 1s and 0s (binary)
+
+## Pro
+* Space efficient
+* Very fast to retrieve
+
+## Cons
+* False negative
