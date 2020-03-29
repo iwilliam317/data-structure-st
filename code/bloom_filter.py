aae34a9fa8c41883dd2e6604246fdf1b954fa1cd
Shinji Obana <willshinji@gmail.com>
2020-03-29 18:59:42 +0100

    bloom filter

--- code/bloom_filter.py
+++ code/bloom_filter.py
@@ -1,28 +1,31 @@
-import pyhash 
+import pyhash
 
-class Pokedex():
+class BloomFilter():
     def __init__(self):
-        self.big_vector = [0]*20
+        self.big_vector = [0] * 20
         self.fnv = pyhash.fnv1_32()
         self.murmur = pyhash.murmur3_32()
+    
+    def add_value(self, value):
+        fnv_value = self.fnv(value) % 20
+        murmur_value = self.murmur(value) % 20
 
-    def add_pokemon(self, pokemon):
-        fnv_pokemon = self.fnv(pokemon)%20
-        murmur_pokemon = self.murmur(pokemon)%20
-
-        if self.is_pokemon_saved(fnv_pokemon, murmur_pokemon):
-            print('already got %s!' %(pokemon))
+        if self.is_value_saved(fnv_value, murmur_value):
+            print('%s already there!' %(value))
             return
+        
+        self.big_vector[fnv_value] = 1
+        self.big_vector[murmur_value] = 1
+        print('%s added' %(value))
 
-        self.big_vector[fnv_pokemon] = 1
-        self.big_vector[murmur_pokemon] = 1
-        print('added %s' %(pokemon))
+    def print_big_vector(self):
+        print(self.big_vector)
 
-    def is_pokemon_saved(self, fnv_pokemon, murmur_pokemon):
-        return (self.big_vector[fnv_pokemon] & self.big_vector[murmur_pokemon])
-pokedex = Pokedex()
-pokedex.add_pokemon('pikachu')
-pokedex.add_pokemon('charmander')
-pokedex.add_pokemon('pikachu')
-pokedex.add_pokemon('pikachu')
-pokedex.add_pokemon('charmander')
+    def is_value_saved(self, fnv_value, murmur_value):
+        return self.big_vector[fnv_value] & self.big_vector[murmur_value]
+bloom = BloomFilter()
+bloom.add_value('Table')
+bloom.add_value('Cooker')
+bloom.add_value('Shelf')
+bloom.add_value('Cooker')
+bloom.print_big_vector()
