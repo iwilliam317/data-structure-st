715176d2017f24d6968e76bd88ab856146dbacfd
Shinji Obana <willshinji@gmail.com>
2020-06-14 13:17:45 +0100

    refactoring bloom filter

--- code/bloom_filter.py
+++ code/bloom_filter.py
@@ -2,13 +2,14 @@ from pyhash import murmur3_32, fnv1_32
 
 class BloomFilter():
     def __init__(self):
-        self.big_vector = [0] * 20
+        self.DIVISOR = 20
+        self.big_vector = [0] * self.DIVISOR
         self.fnv = fnv1_32()
         self.murmur = murmur3_32()
     
     def add_value(self, value):
-        fnv_value = self.fnv(value) % 20
-        murmur_value = self.murmur(value) % 20
+        fnv_value = self.fnv(value) % self.DIVISOR
+        murmur_value = self.murmur(value) % self.DIVISOR
 
         if self.is_value_saved(fnv_value, murmur_value):
             print('%s already there!' %(value))
